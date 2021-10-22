208a42527d85468095b7412dc523ad4ddef5bfcf
aronglee <arong16@naver.com>
2021-10-23 01:24:41 +0900

    [수정] app.js

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,28 +2,23 @@ import React from "react";
 
 import { Route } from "react-router-dom";
 
-import {ConnectedRouter} from 'connected-react-router';
-import { history } from '../redux/configureStore';
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configureStore";
 
 import PostList from "../pages/PostList";
-import Login from "../pages/Login"
-import SignUp from "../pages/SignUp"
-import PostWrite from "../pages/PostWrite"
- 
-
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import PostWrite from "../pages/PostWrite";
 
 function App() {
-
   return (
     <React.Fragment>
-      <React.Fragment>
-        <ConnectedRouter history={history}>
-          <Route path="/" exact component={PostList} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/singup" exact component={SignUp} />
-          <Route path="/write" exact component={PostWrite} />
-        </ConnectedRouter>
-      </React.Fragment>
+      <ConnectedRouter history={history}>
+        <Route path="/" exact component={PostList} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/singup" exact component={SignUp} />
+        <Route path="/write" exact component={PostWrite} />
+      </ConnectedRouter>
     </React.Fragment>
   );
 }
